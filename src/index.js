f8e3b2c22389fba12b38de1c792a8b17f65fa014
Taz <pawelberowski@gmail.com>
2023-07-07 00:24:42 +0200

    Added reject if input is not a number

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 const numberInput = document.querySelector('#number-input');
 const acceptButton = document.querySelector('#accept-button');
+
+function createPromiseFromNumberInput(inputElement) {
+  return new Promise(function(resolve, reject) {
+    if (isNaN(inputElement.value)) {
+      reject();
+    }
+    resolve(inputElement);
+  })
+}
+
+if (numberInput && acceptButton) {
+  acceptButton.addEventListener('click', function() {
+    createPromiseFromNumberInput(numberInput)
+      .catch(function() {
+        console.log('It\'s not a number');
+      })
+  })
+}
