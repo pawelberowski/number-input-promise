e06c7aa479ee1f2db1072924cfa52f2cb9fc41d1
Taz <pawelberowski@gmail.com>
2023-07-07 01:00:38 +0200

    Added creating paragraphs after clickong button

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
-const numberInput = document.querySelector('#number-input');
-const acceptButton = document.querySelector('#accept-button');
+const numberInput = document.querySelector("#number-input");
+const acceptButton = document.querySelector("#accept-button");
 
 function createPromiseFromNumberInput(inputElement) {
-  return new Promise(function(resolve, reject) {
-    if (isNaN(inputElement.value)) {
+  const inputValue = +inputElement.value;
+  return new Promise(function (resolve, reject) {
+    if (isNaN(inputValue)) {
       reject();
     }
-    resolve(inputElement);
-  })
+    if (inputValue < 0) {
+      createParagraphWithInputValue(inputValue);
+      setTimeout(function () {
+        reject(inputElement);
+      }, 2000);
+    } else {
+      if (inputValue % 2) {
+        createParagraphWithInputValue(inputValue);
+        setTimeout(function () {
+          resolve(inputElement);
+        }, 5000);
+      }
+      if (!(inputValue % 2)) {
+        createParagraphWithInputValue(inputValue);
+        setTimeout(function () {
+          resolve(inputElement);
+        }, 10000);
+      }
+    }
+  });
+}
+
+function createParagraphWithInputValue(number) {
+  const newParagraph = document.createElement("p");
+  newParagraph.innerHTML = `Promise: ${number}`;
+  document.body.append(newParagraph);
 }
 
 if (numberInput && acceptButton) {
-  acceptButton.addEventListener('click', function() {
-    createPromiseFromNumberInput(numberInput)
-      .catch(function() {
-        console.log('It\'s not a number');
-      })
-  })
+  acceptButton.addEventListener("click", function () {
+    createPromiseFromNumberInput(numberInput).catch(function () {
+      console.log("It's not a number");
+    });
+  });
 }
